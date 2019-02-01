e349b066a2fa1b8effdcbd434163c30e48217098
GerryWen <blog@gerrywen.com>
2019-02-01 10:24:15 +0800

    初始化工程

---
+++ library/traits/controller/Jump.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: GerryWen
+ * Email: blog@gerrywen.com
+ * Blog: blog.gerrywen.com
+ * Date: 2019/2/1
+ * Time: 10:22
+ */
+
+/**
+ * 用法：
+ * class index
+ * {
+ *     use \traits\controller\Jump;
+ *     public function index(){
+ *         $this->error();
+ *         $this->redirect();
+ *     }
+ * }
+ */
+namespace traits\controller;
+
+trait Jump
+{
+
+}
